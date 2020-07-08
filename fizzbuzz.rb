e01b2b2e9be412d452cd3ca46959a46666659da3
candescent-method-1839 <mqu000@citymail.cuny.edu>
2020-07-08 16:39:37 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,13 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+def fizzbuzz(anum)
+  if (anum % 3 == 0 && anum % 5 == 0)
+    puts "FizzBuzz"
+  end
+  if (anum % 3 == 0 && anum % 5 != 0)
+    puts "Fizz"
+  end
+  if (anum % 3 != 0 && anum % 5 == 0)
+    puts "Buzz"
+  end
+end
